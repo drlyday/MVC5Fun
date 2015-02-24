844df5bffbf2664884e12054f85c986e61fb6650
dlyday <dlyday@eprod.com>
2015-02-24 08:12:08 -0600

    Hello Owin & Katana

---
+++ KatanaIntro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+
+namespace KatanaIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Startup
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app">Owin Component which gives access to Katana components</param>
+        public void Configuration(IAppBuilder app)
+        {
+            app..Run(ctx => {ctx.});
+        }
+    }
+}
---
+++ KatanaIntro001/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace KatanaIntro001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string uri = "http://localhost:8080";
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+                Console.WriteLine("Stopping");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app">Provides behavior for </param>
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(ctx => ctx.Response.WriteAsync("Hellow World"));
+        }
+    }
+}
